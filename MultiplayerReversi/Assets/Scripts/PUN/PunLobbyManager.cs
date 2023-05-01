df9eb29a8af5cf02fbdce4dd1f3f95bb291364eb
MikouRay <mikouray@gmail.com>
2023-05-01 21:11:08 +0800

    Make LobbyManager more complete

--- MultiplayerReversi/Assets/Scripts/PUN/PunLobbyManager.cs
+++ MultiplayerReversi/Assets/Scripts/PUN/PunLobbyManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,15 @@ public class PunLobbyManager : MonoBehaviourPunCallbacks
     public UnityEvent onJoinedLobby;
     public UnityEvent onJoinedRoom;
     public UnityEvent onDisconnected;
+
+    public delegate bool StringValidation(string str);
+    public StringValidation roomNameValidation;
+    public StringValidation passwordValidation;
+
+    public delegate string StringModifier(string str);
+    public StringModifier roomNameModifier;
+
+    public List<GameObject> roomInfoListReceivers;
 
     private void Start() {
         if (PhotonNetwork.IsConnected && PhotonNetwork.JoinLobby()) {
@@ -30,28 +40,73 @@ public class PunLobbyManager : MonoBehaviourPunCallbacks
 
     public bool ProcessRoomName(string origin, out string result) {
         if (origin.Length > 0) {
-            result = origin.Trim();
-            return true;
-        } else {
-            result = "new room";
+            origin = origin.Trim();
+            if (roomNameValidation != null && !roomNameValidation(origin)) {
+                Debug.LogWarning("Invalid room name: " + origin);
+                result = "Invalid room name";
+                return false;
+            } else {
+                result = (roomNameModifier != null) ? roomNameModifier(origin) : origin;
+                return true;
+            }
+        } 
+        else {
+            Debug.LogError("The len of room name cannot be zero.");
+            result = "Invalid room name";
             return false;
         }
-        
     }
 
-    public bool CreateRoom(string _roomName) {
+    private bool CreateRoom(string _roomName, RoomOptions options) {
         if (ProcessRoomName(_roomName, out string roomName)) {
-            PhotonNetwork.CreateRoom(roomName);
+            PhotonNetwork.CreateRoom(roomName, options);
             Debug.Log("Create room: " + roomName);
             return true;
-        } else {
-            Debug.LogWarning("Invalid room name: " + roomName);
+        } 
+        else {
             return false;
         }
+    }
+
+    public bool CreateRoom(string _roomName, byte maxPlayerNum = 0, bool hasPassword = false, string password = "") {
+        if ( password.Length <= 0 ||
+            (passwordValidation != null && !passwordValidation(password))) {
+            Debug.LogWarning("Invalid room password: " + password);
+            return false;
+        }
+
+        ExitGames.Client.Photon.Hashtable roomProps = new ExitGames.Client.Photon.Hashtable();
+        roomProps["hasPassword"] = hasPassword;
+        roomProps["password"] = password;
+
+        RoomOptions options = new RoomOptions();
+        options.CustomRoomProperties = roomProps;
+        options.MaxPlayers = maxPlayerNum;
+
+        return CreateRoom(_roomName, options);
     }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList) {
-        
+        List<RoomInfo> roomListWithoutEmpty = new List<RoomInfo>();
+        foreach(var room in roomList) {
+            if (room.PlayerCount > 0) roomListWithoutEmpty.Add(room);
+        }
+        foreach (var receiver in roomInfoListReceivers) {
+            receiver.SendMessage("OnReceiveRoomInfoList", roomListWithoutEmpty);
+        }
+    }
+
+    public bool JoinRoom(string roomName) {
+        if (PhotonNetwork.InRoom) {
+            Debug.LogError("Joining room failed. Already in a room.");
+            return false;
+        }
+        if (PhotonNetwork.JoinRoom(roomName)) {
+            return true;
+        } else {
+            Debug.LogError("Joining room failed.");
+            return false;
+        }
     }
 
     public override void OnJoinedRoom() {
